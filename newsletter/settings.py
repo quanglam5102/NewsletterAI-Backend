9fe68d3b3fc46abceb8d00c151903522e7006b3f
Lam Huynh <quanglam5102@gmail.com>
2024-11-26 19:26:35 -0500

    added allowed host

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://*.vercel.app',
+    'https://testingapp-nine.vercel.app/newsletter',
 ]
 
 CORS_ALLOW_METHODS = [
